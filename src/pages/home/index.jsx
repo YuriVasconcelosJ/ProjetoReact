a36a869741d159331a7d1fbcf4ebdaa730693e58
YuriVasconcelosJ <yurivasconcelosavatar@gmail.com>
2025-02-04 16:22:22 -0300

    Atualização: Estetilização dos elementos do HTML

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,41 +2,50 @@ import './style.css'
 import Trash from '../../assets/trash.svg'
 function Home() {
 
-  // const users = [{
-
-  // }, {
-
-  // }]
-
+  const users = [{
+    id: '2342asdasds',
+    name: 'Rodolfo',
+    age: 33,
+    email: 'rod@email.com',
+  },
+  {
+    id: '2342asdasd456',
+    name: 'Aline',
+    age: 28,
+    email: 'aline@email.com',
+  }
+  ]
 
 
 
   return (
 
-  // Formulário
-  <div className='container'>
-    <form>
+    // Formulário
+    <div className='container'>
+      <form>
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type="text" />
-        <input name='idade' type="number" />
-        <input name='email' type="email" />
-        <button type='button'>Cadastrar</button>   
-    </form>
-
-    <div>
-      <div>
-        <div>
-          <p>Nome:</p>
-          <p>Idade:</p>
-          <p>Email:</p>
+        <input placeholder='Nome' name='nome' type="text" />
+        <input placeholder='Idade' name='idade' type="number" />
+        <input placeholder='E-mail' name='email' type="email" />
+        <button type='button'>Cadastrar</button>
+      </form>
+      {/* Repetição de cards dos Usuários */}
+      {users.map(user => (
+        <div key={user.id} className='card'>
+          <div>
+            <p>Nome:{user.name} </p>
+            <p>Idade: {user.age} </p>
+            <p>Email: {user.email} </p>
+          </div>
+          <button>
+            <img />
+          </button>
         </div>
-        <button>
-          <img  />
-        </button>
-      </div>
-    </div>
 
-  </div>
+      ))}
+
+
+    </div>
   )
 }
 
